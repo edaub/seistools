983581be8f609e91cc53ac4ff8988846e7467ba2
Eric Daub <egdaub@memphis.edu>
2015-05-07 15:00:16 -0500

    added 3d stress change function

--- seistools/stress.py
+++ seistools/stress.py
@@ -18,3 +18,28 @@ def calc_ds_2d(slip, dx, mu, poisson = 0.):
     f *= -mu/(1.-poisson)*np.abs(k)
 
     return np.real(np.fft.ifft(f))    
+
+def calc_ds_3d(slip_x, slip_y, dx, mu, poisson):
+    """
+    Calculates stress change due to slip on a planar fault (using FFT method)
+    Inputs:
+    slip_x = x-displacement (2d array)
+    slip_y = y-displacement (2d array)
+    dx = grid spacing (assumed to be uniform)
+    mu = shear modulus
+    poisson (optional) = poisson's ratio (if mode 2)
+    Returns:
+    static stress change from fault slip
+    """
+    k = np.fft.fftfreq(len(slip), dx)
+    m = np.fft.fftfreq(len(slip[0]), dx)
+
+    kxy, mxy = np.meshgrid(k, m, indexing='ij')
+
+    fx = np.fft.fft2(slip_x)
+    fy = np.fft.fft2(slip_y)
+
+    sx = -mu/2./np.sqrt(kxy**2+mxy**2)*(1./(1.-poisson)*(kxy**2*fx+mxy*kxy*fy)+(mxy**2*fx-mxy*kxy*fy))
+    sy = -mu/2./np.sqrt(kxy**2+mxy**2)*(1./(1.-poisson)*(mxy**2*fy+mxy*kxy*fx)+(kxy**2*fy-mxy*kxy*fx))
+
+    return np.real(np.fft.ifft2(sx)), np.real(np.fft.ifft2(sy))
