a8eda18ebb6f215ecc496ced27b2fb33f6ca174c
Eric Daub <egdaub@memphis.edu>
2017-04-20 14:58:35 -0500

    changed details in calculating normal vectors in coulomb function, lets users choose tangent direction

--- seistools/coulomb.py
+++ seistools/coulomb.py
@@ -1,24 +1,34 @@
 import numpy as np
 
-def coulomb_2d(sxx, sxy, syy, n, mu):
+def coulomb_2d(sxx, sxy, syy, n, mu, orientation=None):
     """
-    Calcualtes coulomb failure criteria for a given normal and friction coefficient
+    Calcualtes coulomb failure criteria for a given normal direction and friction coefficient
     Inputs:
     stress components sxx, sxy, syy (negative in compression), can be arrays
-    n is normal vector for receiver fault (must have length 2), points towards
+    n is normal vector for receiver fault (must have length 2)
     mu (> 0) is friction coefficient
+    orientation (optional) is a string indicating how you would like to compute the tangent vector
+                    In all cases, the hypothetical second tangent vector is in the +z direction
+                    "x" indicates that the tangent vector is defined by n x t1 = z (x is right handed cross product,
+                          chosen so that faults with a purely +x normal will give a tangent in the +y direction).
+                          This means that a positive Coulomb function leads to left lateral slip on the fault
+                    "y" indicates that the tangent vector is defined by t1 x n = z (so that a normal in the +y
+                          direction will give a tangent vector in the +x direction). This means that a positive
+                          Coulomb function leads to right lateral slip on the fault
+                    None indicates that the "y" convention is used (default)
     Returns:
     coulomb failure function
     """
     assert len(n) == 2, "normal vector must have length 2"
-    assert np.abs(np.sqrt(n[0]**2+n[1]**2)-1.) < 1.e-14, "normal vector must be normalized"
+    assert np.isclose(np.sqrt(n[0]**2+n[1]**2),1.), "normal vector must be normalized"
     assert mu >= 0., "mu must be positive"
+    assert (orientation == "x" or orientation == "y" or orientation == None)
     
-    sn, st = rotate_xy2nt_2d(sxx, sxy, syy, n)
+    sn, st = rotate_xy2nt_2d(sxx, sxy, syy, n, orientation)
 
     return st+mu*sn
 
-def rotate_xy2nt_2d(sxx, sxy, syy, n):
+def rotate_xy2nt_2d(sxx, sxy, syy, n, orientation=None):
     """
     Rotates stress components from xy to normal/tangential to given normal vector
     Inputs:
@@ -28,7 +38,8 @@ def rotate_xy2nt_2d(sxx, sxy, syy, n):
     normal and shear stress in rotated coordinates
     """
     assert len(n) == 2, "normal vector must have length 2"
-    assert np.abs(np.sqrt(n[0]**2+n[1]**2)-1.) < 1.e-14, "normal vector must be normalized"
+    assert np.isclose(np.sqrt(n[0]**2+n[1]**2),1.), "normal vector must be normalized"
+    assert (orientation == "x" or orientation == "y" or orientation == None)
     
     m = tangent_2d(n)
 
@@ -38,17 +49,19 @@ def rotate_xy2nt_2d(sxx, sxy, syy, n):
     return sn, st
 
 
-def tangent_2d(n):
+def tangent_2d(n, orientation=None):
     "Creates returns vector orthogonal to input vector (of length 2)"
     assert len(n) == 2, "normal vector must be of length 2"
-    assert np.abs(np.sqrt(n[0]**2+n[1]**2)-1.) < 1.e-14, "normal vector must be normalized"
+    assert np.isclose(np.sqrt(n[0]**2+n[1]**2),1.), "normal vector must be normalized"
+    assert (orientation == "x" or orientation == "y" or orientation == None)
     
     m = np.empty(2)
-    if np.abs(n[0]) > np.abs(n[1]):
-        m[1] = np.sign(n[0])/np.sqrt(1.+(n[1]/n[0])**2)
-        m[0] = -n[1]/n[0]*m[1]
+    
+    if orientation == "x":
+        m[1] = n[0]/np.sqrt(n[0]**2+n[1]**2)
+        m[0] = -n[1]/np.sqrt(n[0]**2+n[1]**2)
     else:
-        m[0] = -np.sign(n[1])/np.sqrt(1.+(n[0]/n[1])**2)
-        m[1] = -n[0]/n[1]*m[0]
+        m[0] = n[1]/np.sqrt(n[0]**2+n[1]**2)
+        m[1] = -n[0]/np.sqrt(n[0]**2+n[1]**2)
 
     return m
